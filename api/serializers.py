95d77861076d94670322510758488d2957aadd26
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2021-02-06 18:03:01 +0800

    connect with MySQL db

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import fields
-from rest_framework import serializers
-from .models import News
-
-class NewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = News
-        fields = ['headline','source']
