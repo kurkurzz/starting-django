64b30f46c452b6617f60619a70e32aeae35085e7
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2021-02-07 03:17:16 +0800

    implement class based view

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
     path(r'', views.author, name='author'),
-    path(r'news/', views.news, name='news'),
-    path(r'news/<str:pk>', views.news_details, name='news detail'),
+    #function based view
+    # path(r'news/', views.news, name='news'),
+    # path(r'news/<str:id>', views.news_details, name='news detail'),
+    #class based view
+    path(r'news/', views.NewsAPIView.as_view(), name='news'),
+    path(r'news/<str:id>', views.NewsDetailsAPIView.as_view(), name='news detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,13 @@
-
-from rest_framework import serializers
-
+from django.http.response import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from .serializers import NewsSerializer
 from .models import News
 
-# Create your views here.
+#function based view
 @api_view(['GET'])
 def author(request):
     main = {
@@ -34,20 +33,69 @@ def news(request):
         serializer = NewsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data,status.HTTP_201_CREATED)
+            return Response(serializer.data,status.HTTP_201_CREATED)
+        return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
-def news_details(request, pk):
-    news = News.objects.get(id=pk)
-    if request.method=='GET':
+def news_details(request, id):
+    try:
+        news = News.objects.get(id=id)
+        if request.method=='GET':
+            serializer = NewsSerializer(news)
+            return Response(serializer.data,status.HTTP_200_OK)
+        elif request.method=='PUT':
+            serializer = NewsSerializer(news,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status.HTTP_202_ACCEPTED)
+            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+        elif request.method=='DELETE':
+            news.delete()
+            return Response(status.HTTP_204_NO_CONTENT)
+    except:
+        return HttpResponse(status.HTTP_404_NOT_FOUND)
+
+#class based view
+class NewsAPIView(APIView):
+    def get(self, request):
+        news = News.objects.all()
+        serializer = NewsSerializer(news,many=True)
+        return Response(serializer.data,status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status.HTTP_201_CREATED)
+        return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+class NewsDetailsAPIView(APIView):
+    def get_news(self, id):
+        try:
+            return News.objects.get(id=id)
+        except:
+            return HttpResponse(status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        news = self.get_news(id)
+        if type(news) is HttpResponse:
+            return news
         serializer = NewsSerializer(news)
         return Response(serializer.data,status.HTTP_200_OK)
-    elif request.method=='PUT':
+
+    def put(self, request, id):
+        news = self.get_news(id)
+        if type(news) is HttpResponse:
+            return news
         serializer = NewsSerializer(news,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status.HTTP_202_ACCEPTED)
         return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
-    elif request.method=='DELETE':
+    
+    def delete(self, request, id):
+        news = self.get_news(id)
+        if type(news) is HttpResponse:
+            return news
         news.delete()
         return Response(status.HTTP_204_NO_CONTENT)
